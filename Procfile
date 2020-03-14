a43c4daa15a6ac9dc6e28ade651d74ab19f103ee
adesiatnykov <adesiatnykov@sequans.com>
2020-03-15 01:36:38 +0200

    Migration attempt

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
 web: gunicorn --worker-class eventlet -w 1 main:flask_app
+
+init: python manage.py db init
+migrate: python manage.py db migrate
+upgrade: python manage.py db upgrade
--- db/db_config.py
+++ db/db_config.py
@@ -1,18 +1,14 @@
 from helpers import get_env_variable
 
-
 # POSTGRES_URL = get_env_variable("POSTGRES_URL")
 POSTGRES_URL = get_env_variable("DATABASE_URL")
 # POSTGRES_USER = get_env_variable("POSTGRES_USER")
 # POSTGRES_PW = get_env_variable("POSTGRES_PW")
 # POSTGRES_DB = get_env_variable("POSTGRES_DB")
 
-# DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL,
-                                      #                        db=POSTGRES_DB)
+DB_URL = 'postgres://voheseigswrsbm:cc68d8363f89bddabbe1ce4735a7020217b0f96c229854316aaf7592e7fd385c@ec2-3-234-169-1' \
+         '47.compute-1.amazonaws.com:5432/d97uq4qkakfic0'
 
-prefix = 'postgresql+psycopg2'
-
-SQLALCHEMY_DATABASE_URI = prefix + POSTGRES_URL[8:]
+SQLALCHEMY_DATABASE_URI = DB_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'some-secret-string'
-
