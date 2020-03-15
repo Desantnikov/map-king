703ed3e9a59b8c854c8ae1fca32e1fdeeb19f7fe
adesiatnykov <adesiatnykov@sequans.com>
2020-03-15 02:05:17 +0200

    Migration attempt

--- manage.py
+++ manage.py
@@ -1,17 +1,19 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from db.models.user_model import UserModel
 
 from app import flask_app as app, db
 
 
 #app.config.from_object(os.environ['APP_SETTINGS'])
-
-migrate = Migrate(app, db)
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
+# ... some more code here ...
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
+    db.create_all()
