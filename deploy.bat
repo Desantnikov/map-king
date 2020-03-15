4a4d3ccb6e805ec3104c7c4f06af4eda7286c13a
adesiatnykov <adesiatnykov@sequans.com>
2020-03-15 02:47:00 +0200

    Mig att

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,7 @@
 IF %1==master (SET branch=heroku) ELSE (SET branch=dev)
 SET message=%2
 IF [%message%] == [] (SET message=default) ELSE (SET message=%message%)
-IF %~3 == amend (SET key=-f) 
+IF %~3 == amend (SET key=-f)
 
 git add .
 git commit --amend -m "%message%"
