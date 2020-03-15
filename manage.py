3e9ae5f46ea38865342484ef470e5551c18af3d9
adesiatnykov <adesiatnykov@sequans.com>
2020-03-15 13:40:31 +0200

    Migration +tokens tables

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from db.models.user import UserModel
+from db.models.revoked_token import RevokedAccessTokenModel, RevokedRefreshTokenModel
 
 from app import flask_app as app, db
 
